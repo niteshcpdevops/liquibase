43f6679be94ad8cfa09fbf4f5d2d8ac2b9b5bb83
root <root@ip-172-31-9-27.ap-south-1.compute.internal>
2020-01-19 14:55:28 +0000

    liquibase files

---
+++ src/main/java/myself/liquibase/App.java
@@ -0,0 +1,13 @@
+package myself.liquibase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
